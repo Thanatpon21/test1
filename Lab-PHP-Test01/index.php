9829490c14ae08330e792230f0d3d2dd7d2c2f4a
Thanatpon21 <119563370+Thanatpon21@users.noreply.github.com>
2023-03-15 18:22:50 +0700

    Add files via upload

---
+++ Lab-PHP-Test01/index.php
@@ -0,0 +1,21 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+   <?php echo '<p>My name is Itthiphol</p>'; ?> 
+     <?php echo '<p>Update-3st</p>'; ?> 
+     <?php echo '<p>Update-4st</p>'; ?> 
+  <?php echo '<p>Update-555555555555st</p>'; ?> 
+  
+  <?php echo '<p>Update-999888888</p>'; ?> 
+  <?php echo '<p>Update-999888888</p>'; ?> 
+  <?php echo '<p>Update-999888888</p>'; ?> 
+  <?php echo '<p>Update-999888888</p>'; ?> 
+  <?php echo '<p>Update-999888888</p>'; ?> 
+  
+  
+  
+ </body>
+</html>
